752e1ca2ade05218b3e64326f14ef9961485ed9e
FalKebab <143852075+naufal-invictus@users.noreply.github.com>
2025-05-13 21:37:01 +0700

    Update page.tsx

--- app/projects/quote-generator/page.tsx
+++ app/projects/quote-generator/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { toPng } from "html-to-image"
 import { useState, useRef } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
