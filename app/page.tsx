7e8c92466aff6927e0d8fcee47cf5616d6e4a1cb
FalKebab <143852075+naufal-invictus@users.noreply.github.com>
2025-05-19 17:59:01 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -367,7 +367,7 @@ export default function Home() {
             >
               <div className="flex items-center mb-4">
                 <div className="w-12 h-12 bg-secondary/20 rounded-full flex items-center justify-center mr-4">
-                  <span className="text-secondary font-bold">MJ</span>
+                  <span className="text-secondary font-bold">BG</span>
                 </div>
                 <div>
                   <h4 className="font-semibold">Bags</h4>
