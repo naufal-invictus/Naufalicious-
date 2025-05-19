b157aa43c405bfce546f4b9fd95940516fcf6e0b
FalKebab <143852075+naufal-invictus@users.noreply.github.com>
2025-05-19 11:05:57 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
             </p>
           </div>
           <Link href="https://fb-cover-generator.vercel.app/" target="_blank" rel="noopener noreferrer" className="mt-4">
-            <Button variant="secondary" size="md" className="w-full">
+            <Button type="button" size="md" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">
               View Project
             </Button>
           </Link>
@@ -114,7 +114,7 @@ export default function Home() {
             </p>
           </div>
           <Link href="/project/scp-card-generator" target="_blank" rel="noopener noreferrer" className="mt-4">
-            <Button variant="secondary" size="md" className="w-full">
+            <Button type="button" size="md" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">
               View Project
             </Button>
           </Link>
@@ -138,7 +138,7 @@ export default function Home() {
             </p>
           </div>
           <Link href="https://idcard-generator-lyart.vercel.app/mbti" target="_blank" rel="noopener noreferrer" className="mt-4">
-            <Button variant="secondary" size="md" className="w-full">
+            <Button type="button" size="md" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">
               View Project
             </Button>
           </Link>
